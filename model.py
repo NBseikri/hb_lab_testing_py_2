b3856e8145ae2320f5a2feb01f20bc3e9b4a074b
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-28 11:49:17 -0700

    Added functionality to route and database tests. Passed all four tests.

---
+++ model.py
@@ -0,0 +1,40 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Board game."""
+
+    __tablename__ = "games"
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(100))
+
+
+def connect_to_db(app, db_uri="postgresql:///games"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
+
+
+def example_data():
+    """Create example data for the test database."""
+    #FIXME: write a function that creates a game and adds it to the database.
+    # print "FIXME"
+
+    game1 = Game(name='Ticket to Ride', description='a cross-country train adventure')
+    game2 = Game(name='Power Grid', description='supply the most cities with power')
+    game3 = Game(name='Amazing Labyrinth', description='move around the shifting paths of the labyrinth in a race to collect various treasures')
+    game4 = Game(name='Princes of Florence', description='attract artists and scholars trying to become the most prestigious family in Florence')
+    game5 = Game(name='Agricola', description='farmers sow, plow the fields, collect wood, and feed their families')
+
+    db.session.add_all([game1, game2, game3, game4, game5])
+    db.session.commit()
+
+if __name__ == '__main__':
+    from server import app
+
+    connect_to_db(app)
+    print "Connected to DB."
+ 
